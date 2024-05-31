d4a5651df239ae938d44e0ac4cee161051343721
Federica <fedecprinzi@gmail.com>
2024-05-31 19:48:50 +0200

    upload_react_app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# uncontrolledforms_react
+Attempt to access the values of the form by using the FormData API.
+What are the advantages? What are the disadvantages?
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Form from "./components/Form";
+/* Questo esercizio riguarda sia la consegna del "controlled forms - 2" che quella "controlled forms - 5" */
+const App = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
